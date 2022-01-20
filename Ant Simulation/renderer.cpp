c22d34eae61060e87c2b67c6766ebc6709db65e1
caspa <casparjuliusson@gmail.com>
2022-01-20 14:17:30 +0100

    add project

---
+++ Ant Simulation/renderer.cpp
@@ -0,0 +1,43 @@
+static void 
+clear_screen(u32 color) {
+	u32* pixel = (u32*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+static void
+draw_rect_in_pixels(int x0, int y0, int x1, int y1, u32 color) {
+
+	x0 = clamp(0, x0, render_state.width);
+	x1 = clamp(0, x1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
+	
+	for (int y = y0; y < y1; y++) {
+		u32* pixel = (u32*)render_state.memory + x0 + y*render_state.width;
+		for (int x = x0; x < x1; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+static void
+draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
+	
+	x *= render_state.width / window_start_width;
+	y *= render_state.height / window_start_height;
+	half_size_x *= render_state.width / window_start_width;
+	half_size_y *= render_state.height / window_start_height;
+
+	//change to pixels
+	int x0 = x - half_size_x;
+	int x1 = x + half_size_x;
+	int y0 = y - half_size_y;
+	int y1 = y + half_size_y;
+
+	draw_rect_in_pixels(x0, y0, x1, y1, color);
+}
+
