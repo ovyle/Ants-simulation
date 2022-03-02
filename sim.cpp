3ce63c5a31b97b93bee8b9d20a6f476a8731111c
Caspar Juliusson <41427217+ovyle@users.noreply.github.com>
2022-03-02 12:55:22 +0100

    Update and rename sim1.cpp to sim.cpp

--- sim.cpp
+++ sim.cpp
@@ -1,12 +1,24 @@
-
+// Constants
 const float ant_home_x = window_start_width / 2;
 const float ant_home_y = window_start_height / 2;
-const int memory_constant = 2000; //max distance from last target = memory_frames * velocity
-int home_food_count = 0;
+const int memory_constant = 2000; //max distance from last target = memory_iterations * velocity
+const int pheromone_max_strenght = 10000;
+const int food_count_start = 100;
+const int number_of_ants = 100; 
+const int pheromones_per_ant = 15;
+const int pheromone_max_frames = 2000;
+const float sight_angle = PI * 0.25f;
+const float ant_velocity = 0.1f;
 
-// Ants constants
-const float sight_angle = PI * 0.25f, sight_radius = 30* common_constant, reach_radius = 10* common_constant, pheromone_radius = 40* common_constant, ant_velocity = 0.1f* common_constant;
-const int number_of_ants = 40, pheromones_per_ant = 15, pheromone_max_frames = 2000; // have like constant in name
+// Constants changed between simulation settings 
+const float sight_radius = 40, scent_radius = 40, reach_radius = 10;
+
+// Variables
+int home_food_count = 0;
+bool data_save = false;
+int iteration_count = 0;
+int simulation_count = 0;
+
 
 class ants
 {
@@ -15,7 +27,8 @@ public:
 	float y;
 	float rotation;
 	int food_seen_id;
-	int memory_frames;
+	int memory_iterations;
+	int pheromone_strenght;
 	bool seen_target;
 	bool centered_target;
 	bool carrying_food;
@@ -24,7 +37,8 @@ public:
 		x = ant_home_x;
 		y = ant_home_y;
 		rotation = rand() % 100 * PI / 50;
-		memory_frames = memory_constant;
+		memory_iterations = memory_constant;
+		pheromone_strenght = pheromone_max_strenght;
 		seen_target = false;
 		centered_target = false;
 		carrying_food = false;
@@ -81,7 +95,7 @@ struct foods
 		r = ant_home_y / number_of_foods * (n + 1) - 10;
 		x = ant_home_x + rand() % (2 * r + 1) - r; // cirkel equation
 		y = ant_home_y + positive_negative * sqrtf(-powf(ant_home_x, 2) + 2 * ant_home_x * x + powf(r, 2) - powf(x, 2));
-		food_count = 30;
+		food_count = food_count_start;
 	}
 };
 
@@ -89,11 +103,14 @@ struct pheromones
 {
 	float x;
 	float y;
+	int strenght;
 	bool from_home;
 
-	void create(float ant_x, float ant_y, bool carrying_food) {
+
+	void create2(float ant_x, float ant_y, int pheromone_strength, bool carrying_food) {
 		x = ant_x;
 		y = ant_y;
+		strenght = pheromone_strength;
 		if (!carrying_food) {
 			from_home = true;
 		}
@@ -118,23 +135,26 @@ simulation_startup() {
 }
 
 int current_pheromone = 0;
-int pheromone_timer = pheromone_max_frames / pheromones_per_ant; 
-
+int pheromone_create_timer = pheromone_max_frames / pheromones_per_ant; 
+int pheromone_check_timer_constant = 5;
+int pheromone_check_timer = pheromone_check_timer_constant;
 
 static void
 simulation() {
+	iteration_count++;
+
 	float distance = 0;
 	float angle = 0;
 	float tactile_angle;
 	float pheromone_angle;
+
 	// Creating pheromones
-	if (pheromone_timer == 0) {
+	if (pheromone_create_timer == 0) {
 		for (int n = 0; n < number_of_ants; n++) {
-			pheromone[n * pheromones_per_ant + current_pheromone].create(ant[n].x, ant[n].y, ant[n].carrying_food);
+			pheromone[n * pheromones_per_ant + current_pheromone].create2(ant[n].x, ant[n].y, ant[n].pheromone_strenght, ant[n].carrying_food);
 		}
 		current_pheromone++;
 	}
-	// fix that they follow wrong or all trails mabye cause pheromone_angle is shared
 
 	// Ants behavior
 	for (int n = 0; n < number_of_ants; n++) {
@@ -160,21 +180,25 @@ simulation() {
 				// Tactile signals
 				for (int m = 0; m < number_of_ants; m++)
 				{
-					if (m != n && ant[m].carrying_food != ant[n].carrying_food && get_distance(ant[n].x, ant[n].y, ant[m].x, ant[m].y) <= reach_radius && ant[m].memory_frames > 0) {
+					if (m != n && ant[m].carrying_food != ant[n].carrying_food && get_distance(ant[n].x, ant[n].y, ant[m].x, ant[m].y) <= reach_radius && ant[m].memory_iterations > 0) {
 						tactile_angle = get_angle(get_delta_x(ant[n].x, food[ant[m].food_seen_id].x), get_delta_y(ant[n].y, food[ant[m].food_seen_id].y), distance, ant[n].rotation);
 					}
 				}
 				// Pheromone sense
-				for (int m = 0; m < number_of_ants * pheromones_per_ant; m++) { // dont always check
-					if (!pheromone[m].from_home) {
-						distance = get_distance(ant[n].x, ant[n].y, pheromone[m].x, pheromone[m].y); // Swallows up 70 % maybe square check first then that why does it do it in start
-						if (distance <= pheromone_radius) {
-							float sense_angle = get_angle(get_delta_x(ant[n].x, pheromone[m].x), get_delta_y(ant[n].y, pheromone[m].y), distance, ant[n].rotation);
-							if (sense_angle <= abs(pheromone_sense_angle)) {
-								pheromone_angle = pheromone_function(sense_angle);
+				if (pheromone_check_timer == 0) {
+					for (int m = 0; m < number_of_ants * pheromones_per_ant; m++) { // dont always check
+						if (!pheromone[m].from_home) {
+							distance = get_distance(ant[n].x, ant[n].y, pheromone[m].x, pheromone[m].y); // Swallows up 70 % maybe square check first then that why does it do it in start
+							if (distance <= scent_radius) {
+								float sense_angle = get_angle(get_delta_x(ant[n].x, pheromone[m].x), get_delta_y(ant[n].y, pheromone[m].y), distance, ant[n].rotation);
+								if (abs(sense_angle) <= pheromone_sense_angle) {
+									pheromone_locate(sense_angle, pheromone[m].strenght);
+								}
 							}
 						}
 					}
+					pheromone_angle = pheromone_direction();
+					pheromone_concentration_reset();
 				}
 			} // Pick up food
 			else {
@@ -183,7 +207,9 @@ simulation() {
 					ant[n].carrying_food = true;
 					ant[n].seen_target = false;
 					ant[n].centered_target = false;
-					ant[n].memory_frames = memory_constant;
+					ant[n].memory_iterations = memory_constant;
+					ant[n].pheromone_strenght = pheromone_max_strenght;
+					ant[n].rotation += PI;
 				}
 			}
 		} 
@@ -201,21 +227,25 @@ simulation() {
 				//tactile signals
 				for (int m = 0; m < number_of_ants; m++)
 				{
-					if (m != n && ant[m].carrying_food != ant[n].carrying_food && get_distance(ant[n].x, ant[n].y, ant[m].x, ant[m].y) <= reach_radius && ant[m].memory_frames > 0) {
+					if (m != n && ant[m].carrying_food != ant[n].carrying_food && get_distance(ant[n].x, ant[n].y, ant[m].x, ant[m].y) <= reach_radius && ant[m].memory_iterations > 0) {
 						tactile_angle = get_angle(get_delta_x(ant[n].x, ant_home_x), get_delta_y(ant[n].y, ant_home_y), distance, ant[n].rotation);
 					}
 				}
 				// Pheromone sense
-				for (int m = 0; m < number_of_ants * pheromones_per_ant; m++) {
-					if (pheromone[m].from_home) {
-						distance = get_distance(ant[n].x, ant[n].y, pheromone[m].x, pheromone[m].y);
-						if (distance <= pheromone_radius) {
-							float sense_angle = get_angle(get_delta_x(ant[n].x, pheromone[m].x), get_delta_y(ant[n].y, pheromone[m].y), distance, ant[n].rotation);
-							if (sense_angle <= abs(pheromone_sense_angle)) {
-								pheromone_angle = pheromone_function(sense_angle);
+				if (pheromone_check_timer == 0) {
+					for (int m = 0; m < number_of_ants * pheromones_per_ant; m++) {
+						if (pheromone[m].from_home) {
+							distance = get_distance(ant[n].x, ant[n].y, pheromone[m].x, pheromone[m].y);
+							if (distance <= scent_radius) {
+								float sense_angle = get_angle(get_delta_x(ant[n].x, pheromone[m].x), get_delta_y(ant[n].y, pheromone[m].y), distance, ant[n].rotation);
+								if (abs(sense_angle) <= pheromone_sense_angle) {
+									pheromone_locate(sense_angle, pheromone[m].strenght);
+								}
 							}
 						}
 					}
+					pheromone_angle = pheromone_direction();
+					pheromone_concentration_reset();
 				}
 			}
 			// drop off food
@@ -225,25 +255,41 @@ simulation() {
 					ant[n].carrying_food = false;
 					ant[n].seen_target = false;
 					ant[n].centered_target = false;
-					ant[n].memory_frames = memory_constant;
+					ant[n].memory_iterations = memory_constant;
+					ant[n].pheromone_strenght = pheromone_max_strenght;
+					ant[n].rotation += PI;
+
+					if (home_food_count == food_count_start* number_of_foods) {
+						data_save = true;
+					}
+					if (home_food_count == 225) { // test food_count_start * number_of_foods*0.75
+						data_save = true;
+					}
 				}
 			}
 		}
 		// Reducing ants memory
-		if (ant[n].memory_frames > 0) {
-			ant[n].memory_frames--; // could have one for all also iterations instead of frames
+		if (ant[n].memory_iterations > 0) {
+			ant[n].memory_iterations--; // could have one for all also iterations instead of frames
 		}
-		
+		if (ant[n].pheromone_strenght > 1500) {
+			ant[n].pheromone_strenght--;
+		}
+
 		// Rotation and movement
 		ant[n].set_rotation(angle, tactile_angle, pheromone_angle);
 		ant[n].collision();
 		ant[n].movement();
 	}
 
-	// changing pheromones variables that depend on frames
-	pheromone_timer--;
-	if (pheromone_timer < 0) {
-		pheromone_timer = pheromone_max_frames / pheromones_per_ant;
+	// changing pheromones variables that depend on iterations
+	pheromone_create_timer--;
+	pheromone_check_timer--;
+	if (pheromone_create_timer < 0) {
+		pheromone_create_timer = pheromone_max_frames / pheromones_per_ant;
+	}
+	if (pheromone_check_timer < 0) {
+		pheromone_check_timer = pheromone_check_timer_constant;
 	}
 	if (current_pheromone >= pheromones_per_ant) {
 		current_pheromone = 0;
@@ -253,21 +299,29 @@ simulation() {
 	clear_screen(0x051019);
 	for (int n = 0; n < number_of_ants * pheromones_per_ant; n++) {
 		if (pheromone[n].from_home) {
-			draw_rect(pheromone[n].x, pheromone[n].y, 1* common_constant, 1* common_constant, 0x260dae);
+			draw_rect(pheromone[n].x, pheromone[n].y, 1, 1, 0x260dae);
 		}
 		else {
-			draw_rect(pheromone[n].x, pheromone[n].y, 1* common_constant, 1* common_constant, 0x10ff10);
+			draw_rect(pheromone[n].x, pheromone[n].y, 1, 1, 0x10ff10);
 		}
 	}
 	for (int n = 0; n < number_of_ants; n++) {
-		draw_rect(ant[n].x, ant[n].y, 4* common_constant, 4* common_constant, 0xff0000);
+		draw_rect(ant[n].x, ant[n].y, 4, 4, 0xff0000);
 	}
 	for (int n = 0; n < number_of_foods; n++) {
 		if (food[n].food_count > 0) {
-			draw_rect(food[n].x, food[n].y, 10* common_constant, 10* common_constant, 0x10ff10);
+			draw_rect(food[n].x, food[n].y, 10, 10, 0x10ff10);
 		}
 	}
-	draw_rect(ant_home_x, ant_home_y, 10* common_constant, 10* common_constant, 0x260dae);
+	draw_rect(ant_home_x, ant_home_y, 10, 10, 0x260dae);
+}
+
+void variable_reset() {
+	home_food_count = 0;
+	current_pheromone = 0;
+	for (int n = 0; n < number_of_ants * pheromones_per_ant; n++) {
+		pheromone[n].from_home = true;
+		pheromone[n].x = ant_home_x;
+		pheromone[n].y = ant_home_y;
 	}
-
-
+}
